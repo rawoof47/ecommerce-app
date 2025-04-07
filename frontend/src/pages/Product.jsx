d73f385b53f546e349e9b72ca6003bd6df0384e5
rawoof47 <i.rawoof4@gmail.com>
2025-04-07 18:12:52 +0530

    addded related items

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,13 +2,15 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   const { productId } = useParams();
   const { products, currency } = useContext(ShopContext);
-
   const [productData, setProductData] = useState(null);
   const [image, setImage] = useState('');
+  const [size, setSize] = useState('');
+
 
   useEffect(() => {
     const foundProduct = products.find((item) => item._id === productId);
@@ -58,17 +60,35 @@ const Product = () => {
             <p>Select Size</p>
             <div className='flex gap-2'>
               {productData.sizes.map((item, index)=>(
-                <button className={`border py-2 px-4 bg-gray-100`} key={index}>{item}
+                <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}
                 </button>
               ))}
 
             </div>
 
           </div>
-
-
+          <button className='bg-black text-white font-medium py-3 px-8 active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
       </div>
+      {/* description and review section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>Horizon – Your ultimate destination for premium clothing and accessories. Stay ahead of trends, shop effortlessly, and redefine your wardrobe with timeless fashion. Enjoy exclusive deals, fast shipping, and a seamless shopping experience designed just for you</p>
+          <p>A clean and modern fashion destination offering curated collections for men, women, and kids. With easy navigation, powerful search, and smart filters, this e-commerce platform delivers a smooth and enjoyable shopping experience from start to finish.</p>
+        </div>
+      </div>
+      {/* display related products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
     </div>
   ) : (
     <div className='opacity-0'>Loading...</div>
