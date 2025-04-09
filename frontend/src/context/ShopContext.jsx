46c676944ecea617603f8a3c1c466305c4b50f95
rawoof47 <i.rawoof4@gmail.com>
2025-04-09 11:52:25 +0530

    crreated api getcartamount

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -60,6 +60,31 @@ const ShopContextProvider = (props) => {
 
   }
 
+  const getCartAmount = async () => {
+
+    let totalAmount = 0;
+    for(const items in cartItems){
+
+      let itemInfo = products.find((product)=>product._id === items); 
+      for(const item in cartItems[items]){
+        try {
+          if (cartItems[items][item] > 0) {
+
+            totalAmount += itemInfo.price * cartItems[items][item]
+            
+          }
+          
+        } catch (error) {
+          
+        }
+
+      }
+
+    }
+    return totalAmount;
+
+  }
+
   const value = {
     products,
     currency,
